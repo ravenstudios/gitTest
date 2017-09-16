38e2aec58da3606fd1b30c4ed9bb6e0661f3799d
ravenstudios <ravenstudiosrob@yahoo.com>
2017-09-15 23:20:14 -0500

    added rough functionality to get notes of the major scale. Need to add other scales

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 console.log(" GIT test");
 
+let major = [2, 2, 1, 2, 2, 2, 1];
+let notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 $(function(){
-  console.log("jquery ready");
+
+  $("#root").change(()=>{
+    console.log("change");
+    let scale = "";
+    let scaleSize = major.length;
+
+
+    let startingNote = notes.indexOf($("#root").val());
+
+    scale += notes[startingNote] + " ";
+    let currentNote = startingNote;
+    for (var i = 0; i < scaleSize; i++) {
+      currentNote += major[i];
+      console.log(currentNote);
+
+      scale += notes[currentNote % 12]  + " ";
+    }
+    console.log(scale);
+  });
 })
