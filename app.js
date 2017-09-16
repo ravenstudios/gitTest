5729b6eaec8f4aada84ce3d9d2ce4d9ce477e409
ravenstudios <ravenstudiosrob@yahoo.com>
2017-09-15 23:50:49 -0500

    refractored to allow more scales and display in the p tag

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
 console.log(" GIT test");
-
-let major = [2, 2, 1, 2, 2, 2, 1];
 let notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+let major =    [0, 2, 2, 1, 2, 2, 2, 1];
+let natMinor = [0, 2, 1, 2, 2, 1, 2, 2];
+let harMinor = [0, 2, 1, 2, 2, 1, 3, 1];
+let scales = [major, natMinor, harMinor];
+
+
+// int majScaleSteps[] = {0,2,2,1,2,2,2,1};
+//    int natMinScaleSteps[] = {0,2,1,2,2,1,2,2};
+//    int harMinScaleSteps[] = {0,2,1,2,2,1,3,1};
+
+
 $(function(){
-
+  getNotes();
   $("#root").change(()=>{
-    console.log("change");
-    let scale = "";
-    let scaleSize = major.length;
-
-
-    let startingNote = notes.indexOf($("#root").val());
-
-    scale += notes[startingNote] + " ";
-    let currentNote = startingNote;
-    for (var i = 0; i < scaleSize; i++) {
-      currentNote += major[i];
-      console.log(currentNote);
-
-      scale += notes[currentNote % 12]  + " ";
-    }
-    console.log(scale);
+    // console.log("change");
+    // let scale = "";
+    // let scaleSize = major.length;
+    //
+    //
+    // let startingNote = notes.indexOf($("#root").val());
+    //
+    // scale += notes[startingNote] + " ";
+    // let currentNote = startingNote;
+    // for (var i = 0; i < scaleSize; i++) {
+    //   currentNote += major[i];
+    //   console.log(currentNote);
+    //
+    //   scale += notes[currentNote % 12]  + " ";
+    // }
+    // console.log(scale);
+    //
+    //
+    //
+    // getNotes(root, scale, numOfSteps);
+    getNotes();
   });
+  $("#scale").change(() => {
+    getNotes();
+  });
+
 })
+
+
+function getNotes() {
+  let result = [];
+  let root = notes.indexOf($("#root").val());;
+  let scale = scales[$("#scale").val()];
+  let numOfSteps = scale.length;
+  let currentNote = root;
+  for (var i = 0; i < numOfSteps; i++) {
+    currentNote += scale[i];
+    result.push(notes[currentNote % 12])
+  }
+  displayNotes(result);
+}
+
+function displayNotes(array) {
+  $("#notes").html(array.split(" "));
+}
